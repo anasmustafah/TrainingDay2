ca97de553ee17aa4c63919158fd7935ea278972d
AnasMM <mep11abm@shef.ac.uk>
2015-03-18 16:20:29 +0000

    added exercise after coffee

--- Example1.m
+++ Example1.m
@@ -18,10 +18,52 @@ yClean = y(~missingVals);
 %Formulate the model and write it in MATLAB code
 %We can write this as pulse pressure = [1, Age, Age^2]*{c0 ; c1; c2]
 b = yClean;
-A = [ones(size(xClean)), xClean, xClean.^2]; %alternatively than ones, xClean^0
+A = [ones(size(xClean)), xClean, xClean.^2]; %alter than ones,xClean^0
 modelCoeffs = A\b; %solve the system
 pulseModel = A * modelCoeffs;
 %Plot
 hold on
 plot(xClean, pulseModel, 'r*')
+legend('Raw Data','Fitted Model')
+
+%% Move to 2D
+height = MedData.Height; %x1
+waist = MedData.Waist; %x2
+weight = MedData.Weight; %y
+%Problem: can we estimate weight (y) using height and waist (x1 and x2)?
+%We'll use a quadratic model
+
+% Step 1: Plot the raw data (SCATTER3)
+figure
+scatter3(height, waist, weight,'.k')
+xlabel('Height (cm)')
+ylabel('Waist (cm)')
+zlabel('Weight (kg)')
+title('Weight, waist and height relations')
+% Step 2: Clean the data
+unclean = isnan(height) | isnan(waist) | isnan(weight);
+%alter way to do this is unclean = any(isnan([height, weight, waist]);
+weightC = weight(~unclean);
+heightC = height(~unclean);
+waistC = waist(~unclean);
+% Step 3: Solve the system
+b2 = weightC;
+A2 = [weightC.^0, heightC, heightC.^2, waistC, waistC.^2, waistC.*heightC];
+modelCoeffs2 = A2\b2;
+
+weightmodel = A2 * modelCoeffs2;
+
+% Step 4: Plot fitted model
+modelFun = @(c, x1, x2) c(1) +c(2)*x1 + c(3)*x1.^2 + c(4)*x2 + c(5)*x2.^2;
+% substep 1: make vector data for x1 and x2
+heightVec = linspace(min(heightC), max(heightC), 150);
+waistVec = linspace(min(waistC), max(waistC), 150);
+%substep 2: turn this into grid
+[HEIGHT, WAIST] = meshgrid(heightVec, waistVec);
+%substep 3: evaluate model on the grid
+ModelOnGrid = modelFun( modelCoeffs2, HEIGHT, WAIST);
+
+hold on
+surf(HEIGHT, WAIST, ModelOnGrid)
+plot3(heightC, waistC, weightmodel, 'r*')
 legend('Raw Data','Fitted Model')
