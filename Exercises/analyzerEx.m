7885b3a544132ec4c73637cf405fa7b9b66220ff
AnasMM <mep11abm@shef.ac.uk>
2015-03-19 16:56:58 +0000

    Course ended

--- Exercises/analyzerEx.m
+++ Exercises/analyzerEx.m
@@ -1,17 +1,30 @@
-function analyzer_ex
+function [percent_odd, backward_elements] = analyzerEx()
 
 % Generate a 100-by-100 matrix where A(i,j) = i + j;
-for ii = 1:100
-    for jj = 1:100
-        A(ii,jj) = ii + jj
-    end
-    end
-    
+% A = NaN(nchoosek(100,1));
+% for ii = 1:100
+%     for jj = 1:100
+%         A(ii,jj) = ii + jj;
+%     end
+% end
+
+ii = 1:100;
+jj = 1:100;
+% MESHGRID
+[II,JJ] = meshgrid(ii,jj);
+A = II + JJ;
+% Matrix multiplication
+A = ones(100,1)*ii+jj'*ones(1,100);
+% REPMAT
+A = repmat(1:100, 100, 1) + repmat((1:100).',1,100);
+% CUMSUM
+A = cumsum(ones(100)) + cumsum(ones(100),2);
+
 % Calculate the percentage of elements that are odd
 num_pos = sum(rem(A(:), 2) == 1);
-num_elements = prod(size(A);
-percent_odd = num_pos/num_elemetns;
+num_elements = numel(A);
+percent_odd = 100*num_pos/num_elements;
 
 % Create an array of elements in backwards order
-backward_elements = fliplr(sort(A(:)));
+backward_elements = (sort(A(:),'descend'));
 
